2eb040903a8405ecb496b85c4c79d1d93fef6aae
p4panash <muntean.catalin.avram@gmail.com>
2019-12-03 00:11:32 +0200

    Add filter exercise

---
+++ JS/From fundamentals to functional JS/ex6.js
@@ -0,0 +1,98 @@
+const _ = {}
+
+_.filter = function(arr, cb) {
+    result = []
+    for (let i = 0; i < arr.lenght; i++) {
+        if (cb(arr[i], i, arr) === true)
+            result.push(arr[i])
+    }
+    return result;
+}
+
+_.filter = function(arr, cb) {
+    result = []
+    ._each(arr, function(item, i, list) {
+        if (cb(item, i, list) === true)
+            result.push(item);
+    })
+    return result;
+}
+
+const videoData = [
+    {
+        name: 'Miss Scarlet',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Mrs. White',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Reverend Green',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Rusty',
+        present: false,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Colonel Mustard',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    },
+    {
+        name: 'Professor Plum',
+        present: true,
+        rooms: [
+            {kitchen: false},
+            {ballroom: false},
+            {conservatory: false},
+            {'dining room': false},
+            {'billiard room': false},
+            {library: false}
+        ]
+    }
+];
+
+_.filter(videoData, function(suspectObject) {
+    return suspectObject.present;
+});
