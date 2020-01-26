de9c2065b81823157dd5c083d63952d6278d2765
p4panash <muntean.catalin.avram@gmail.com>
2020-01-26 22:55:52 +0200

    Add Details route, custom Babel config, Carousel

--- React/adopt-me/src/Pet.js
+++ React/adopt-me/src/Pet.js
@@ -6,7 +6,7 @@ export default function Pet({ name, animal, breed, media, location, id }) {
   }
 
   return (
-    <a href={`details/id${id}`} className="pet">
+    <a href={`details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
